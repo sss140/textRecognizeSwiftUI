b7477b562e98255063904829206b636275a963d8
佐藤一成 <kazunarisato0309@gmail.com>
2020-06-05 04:11:52 +0900

    Initial Commit

---
+++ textRecognizeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  textRecognizeSwiftUI
+//
+//  Created by 佐藤一成 on 2020/06/05.
+//  Copyright © 2020 s140. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
